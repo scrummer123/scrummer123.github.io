fb42862bc165eff72fae6ddf736068edaa265c55
Simon Peters <simonpeters05@gmail.com>
2020-12-19 00:04:32 +0100

    :wrench: Making watch script with postcss and esbuild

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App: React.FC = () => (
   <div>
     <h1>test</h1>
-    <button className="bg-gray-800 p-1 rounded text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+    <button className="customButton">
       <span className="sr-only">View notifications</span>
     </button>
   </div>
