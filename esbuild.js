6f2ecb3c40fabf1223d4c232d7558d048332a505
Simon Peters <simonpeters05@gmail.com>
2020-12-19 01:52:15 +0100

    :wrench: Searching for css minifiers

--- esbuild.js
+++ esbuild.js
@@ -1,3 +1,4 @@
+const { exec } = require("child_process");
 const esbuild = require("esbuild");
 
 esbuild.serve(
@@ -8,4 +9,14 @@ esbuild.serve(
     sourcemap: true,
     define: { "process.env.NODE_ENV": '"development"' },
   }
-);
+).then(async () => {
+  const watchPostcss = await exec("npm run watch:postcss:index");
+
+  for await (const data of watchPostcss.stdout) {
+    for await (const errData of watchPostcss.stderr) {
+      console.error(`CSS ERROR: ${errData}`);
+    }
+    console.log(data);
+  }
+
+});
