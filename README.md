267bc44002abc50db0bc15f3857319a4a54c8536
simon-instance <simonpeters05@gmail.com>
2021-01-02 14:53:45 +0100

    :zap: Initial nextjs setup

--- README.md
+++
@@ -1,5 +0,0 @@
-# simon-instance.github.io
-
-## App configuration
-
-The entrypoint of the compiled javascript file is in src/EntryPoint. Include the file in an index.html script tag to get started.
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (ctx) => ({
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss': true,
-    'autoprefixer': true,
-    'cssnano': ctx.env === 'production' ? {} : false
-  }
-});
