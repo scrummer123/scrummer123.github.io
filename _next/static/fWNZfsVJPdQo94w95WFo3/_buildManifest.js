240d16d4838b2d381166d5111bf2c46eb2c1f73e
simon-instance <simonpeters05@gmail.com>
2021-01-02 22:42:54 +0100

    Updates

---
+++ _next/static/fWNZfsVJPdQo94w95WFo3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.7d7f2423be0d9776d076.js","static\u002Fchunks\u002Fpages\u002Findex-16222a1561b297d19c6c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-986b9ac52257fbcb5ebd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
